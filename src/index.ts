096bbe8b7927c8388280aa4f53a99c636dca2e59
hung <hungbn0112@gmail.com>
2020-09-23 15:37:23 +0700

    [package] Use lib for output

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { ActionFunc } from 'types/actions'
+
 export * from './action-types'
 export * from './actions'
 export * from './constants'
@@ -7,3 +9,13 @@ export * from './reducers'
 export * from './store'
 // export * from './types'
 // export * from './utils'
+
+declare module 'redux' {
+  /*
+   * Overload to add thunk support to Redux's dispatch() function.
+   * Useful for react-redux or any other library which could use this type.
+   */
+  export interface Dispatch<A extends Action = AnyAction> {
+    <TReturnType = any>(actionFunc: ActionFunc): TReturnType
+  }
+}

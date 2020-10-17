b9bcf2f53130b24eccba809c6bcbf6465d5ae92e
hungbn <masterhung0112@gmail.com>
2020-10-17 12:27:36 +0000

    Remove unused package

--- src/selectors/common.ts
+++ src/selectors/common.ts
@@ -20,17 +20,6 @@ export const getCurrentUser = createSelector(
   (profiles, currentUserId) => profiles[currentUserId]
 )
 
-// export const getCurrentUser(state: UsersAwareState): UserProfile {
-//   return state[USERS_MODULE_NAME].profiles[getCurrentUserId(state)]
-// }
-// export function getCurrentUserId(state: UsersAwareState): string {
-//   return state[USERS_MODULE_NAME].currentUserId
-// }
-
-// export function getUserProfiles(state: UsersAwareState): IDMappedObjects<UserProfile> {
-//   return state[USERS_MODULE_NAME].profiles
-// }
-
 export function getMyChannelMemberships(state: GlobalState): RelationOneToOne<Channel, ChannelMembership> {
   return state.entities.channels.myMembers
 }
